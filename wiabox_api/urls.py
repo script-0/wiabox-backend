8a6ecd298603f7a128b1c4eaaf8d917dfe698426
Isaac NDEMA <ndemabekolleemmanuelisaac@gmail.com>
2021-04-24 22:42:44 +0100

    Modify some routes

--- wiabox_api/urls.py
+++ wiabox_api/urls.py
@@ -9,6 +9,6 @@ router.register(r'community', views.CommunityViewSet)
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    path('api/', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('', include(router.urls)),
+    path('auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
